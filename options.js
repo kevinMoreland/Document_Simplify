f75739ae12023e007b2a12fe0cf19ba3f3a32062
kevinMoreland <kmmorela@calpoly.edu>
2019-11-22 17:49:02 -0800

    can now send email to any address specified. Unfortunately had to remove previous history

--- options.js
+++ options.js
@@ -6,16 +6,18 @@
 
 let page = document.getElementById('buttonDiv');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+
 function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
+    let button = document.getElementById('submitButton');
+    
     button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
+        var nameValue = document.getElementById("email").value;
+        chrome.storage.sync.set({email: nameValue}, function() {
+            console.log('color is ' + nameValue);
       })
     });
+    
     page.appendChild(button);
-  }
 }
+
 constructOptions(kButtonColors);
--- popup.js
+++ popup.js
@@ -6,16 +6,22 @@
 
 let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+
+changeColor.onclick = function(element) {    
+    chrome.storage.sync.get('email', function(data) {
+        changeColor.innerHTML = "Send Email to Kindle: " + data.email;
+        let emailReciever = data.email;
+        Email.send({
+            SecureToken : "f4d23c20-26bf-43c7-8436-bd27d826a6c0",
+            To : emailReciever,
+            From : "documentsimplify@gmail.com",
+            Subject : "Convert",
+            Body : "And this is the body"
+        }).then(
+        message => alert(message)
+    );
+    });
+    
+
 };
